54127cf3da3f10f2a428b4cd3de9491074f162a7
mayaman26 <mayaman26@gmail.com>
2017-09-10 22:32:59 -0700

    first commit

---
+++ WordCount/src/main/java/cs181/WordCount.java
@@ -0,0 +1,42 @@
+package cs181;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class WordCount {
+            
+	public static void main(String[] args) throws Exception {
+		
+		if (args.length < 2) {
+			System.err.println("Usage: hadoop jar /path/to/jar/WordCount-0.0.1-SNAPSHOT.jar <inputPath> <outputPath>" );
+		}
+		Configuration conf = new Configuration();
+	        
+	    Job job = new Job(conf);
+	  
+	    job.setJarByClass(WordCount.class);
+	    
+	    /* Mapper generated Key/Value data types */ 
+	    job.setOutputKeyClass(Text.class); 
+	    job.setOutputValueClass(IntWritable.class);
+	        
+	    job.setMapperClass(WordCountMapper.class);
+	    job.setCombinerClass(WordCountReduce.class); // Combiner is same as reducer
+	    job.setReducerClass(WordCountReduce.class);
+	        
+	    job.setInputFormatClass(TextInputFormat.class);
+	    job.setOutputFormatClass(TextOutputFormat.class);
+	        
+	    FileInputFormat.addInputPath(job, new Path(args[0])); // Location of input
+	    FileOutputFormat.setOutputPath(job, new Path(args[1])); // Location of output
+	        
+	    job.waitForCompletion(true); // Start the job!
+	}
+        
+}
